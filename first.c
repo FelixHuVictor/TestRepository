ff0cae3c1a7a9264c3c3e60679a96765f6bd26c9
felix_hu <335190385@qq.com>
2018-04-26 15:57:41 +0800

    cc编译

--- first.c
+++ first.c
@@ -1,8 +1,7 @@
 /*my first C program*/
 #include<stdio.h>
-int main(){
+void main(){
 	printf("hi there! \n");
 	printf("this is my first C program \n");
 	printf("hello world \n");
-    return 0;
 }
